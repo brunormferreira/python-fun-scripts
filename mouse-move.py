f30ac34864e6bf4edd5b0c91bd2c6e22b38659d4
brunormferreira <brunormferreira@hotmail.com>
2020-04-02 15:37:34 -0300

    first script

---
+++ mouse-move.py
@@ -0,0 +1,33 @@
+def main():
+    import pyautogui
+
+    # Pauses and Fail-Safes
+    pyautogui.PAUSE = 1  # pause 1 second for every pyautogui function call
+    pyautogui.FAILSAFE = True  # cause FailSafeException by moving mouse cursor to upper-left corner
+
+    # Moving the Mouse
+    print(pyautogui.size())
+    print('Running mouse-move')
+    print('Press Ctrl-C to quit.')
+    width, height = pyautogui.size()
+	
+    for i in range(10):
+	pyautogui.click()
+        pyautogui.moveTo(100, 100, duration=0.25)
+        pyautogui.moveTo(200, 100, duration=0.25)
+        pyautogui.moveTo(200, 200, duration=0.25)
+        pyautogui.moveTo(100, 200, duration=0.25)
+
+    for i in range(10):
+        pyautogui.moveRel(100, 0, duration=0.25)
+        pyautogui.moveRel(0, 100, duration=0.25)
+        pyautogui.moveRel(-100, 0, duration=0.25)
+        pyautogui.moveRel(0, -100, duration=0.25)
+
+    # Getting the Mouse Position
+    print(pyautogui.position())
+    print(pyautogui.position())
+    print(pyautogui.position())
+
+if __name__ == '__main__':
+    main()
